56e143b25a983931b49ee03b79720d2edc158393
PinkM0th <98178122+PinkM0th@users.noreply.github.com>
2024-07-15 17:24:32 +0200

    Add ProductView Currency Indicating

--- Assets/Training/Domain/Products/ProductView.cs
+++ Assets/Training/Domain/Products/ProductView.cs
@@ -11,6 +11,8 @@ namespace Training.Domain.Products
         [SerializeField] private TMP_Text _nameText;
         [SerializeField] private TMP_Text _priceText;
         [SerializeField] private Button _buyButton;
+        [SerializeField] private Image coinIcon;
+        [SerializeField] private Image crystalIcon;
 
         public event Action OnBuy;
 
@@ -19,6 +21,17 @@ namespace Training.Domain.Products
             _nameText.text = product.Name;
             _priceText.text = product.Price.ToString();
             _buyButton.onClick.AddListener(() => OnBuy?.Invoke());
+
+            if (product.IsCoinProduct)
+            {
+                coinIcon.enabled = true;
+                crystalIcon.enabled = false;
+            }
+            else
+            {
+                coinIcon.enabled = false;
+                crystalIcon.enabled = true;
+            }
         }
     }
 }
